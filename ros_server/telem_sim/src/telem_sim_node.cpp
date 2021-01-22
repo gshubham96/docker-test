317c8f328c033e26e62386c0c9f882003038e6ea
gshubham96 <shubham.garg@tecnico.ulisboa.pt>
2021-01-22 11:13:30 +0530

    output now serves the relevant message type

--- ros_server/telem_sim/src/telem_sim_node.cpp
+++ ros_server/telem_sim/src/telem_sim_node.cpp
@@ -1,72 +1,50 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/Point.h"
+#include "geometry_msgs/PoseStamped.h"
+#include <GeographicLib/UTMUPS.hpp>
+#include <math.h> 
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 
 #include <sstream>
 
-// from http://phoxis.org/2013/05/04/generating-random-numbers-from-normal-distribution-in-c/
-double randn(double mu, double sigma)
+geometry_msgs::PoseStamped trajSimulator(double r, double th)
 {
-    double U1, U2, W, mult;
-    static double X1, X2;
-    static int call = 0;
-    if (call)
-    {
-        call = !call;
-        return (mu + sigma * (double)X2);
-    }
-    do
-    {
-        U1 = -1 + ((double)rand() / RAND_MAX) * 2;
-        U2 = -1 + ((double)rand() / RAND_MAX) * 2;
-        W = pow(U1, 2) + pow(U2, 2);
-    } while (W >= 1 || W == 0);
+    geometry_msgs::PoseStamped pose;
+    pose.header.stamp = ros::Time::now();
+    pose.header.frame_id = "hrov/rov";
 
-    mult = sqrt((-2 * log(W)) / W);
-    X1 = U1 * mult;
-    X2 = U2 * mult;
-    call = !call;
-    return (mu + sigma * (double)X1);
-}
+    double x = 4290770 + r * cos (th);
+    double y = 491880  + r * sin (th);
+    GeographicLib::UTMUPS::Reverse(29, true, y, x, pose.pose.position.x, pose.pose.position.y);
+    pose.pose.position.z = 2;
 
-ros::Publisher replier_pub;
-void chatterCallback(const std_msgs::String& msg)
-{
-    std::string hello = "hello world, this is medusa from IST: ";
-    std_msgs::String msg_pub;
-    if (msg.data == "marco")
-	msg_pub.data = hello + std::string("polo");
-    else
-	msg_pub.data = hello + msg.data ;
+    tf2::Quaternion quat_tf;
+    quat_tf.setRPY(0, 0, sin(th)); // orientation of the sensor
+    pose.pose.orientation = tf2::toMsg(quat_tf);
 
-    ROS_INFO("Publishing - %s", msg_pub.data.c_str());
-    replier_pub.publish(msg_pub);
+    return pose;
 }
 
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "telem_talker");
+    ros::init(argc, argv, "trajectory_simulator");
     ros::NodeHandle n;
 
-    ros::Subscriber listener_pub = n.subscribe("/hrov/listener", 2, chatterCallback);
-    ros::Publisher talker_pub = n.advertise<geometry_msgs::Point>("/hrov/talker", 10);
-    replier_pub = n.advertise<std_msgs::String>("/hrov/replier", 10);
-    ros::Rate loop_rate(5);
+    ros::Publisher pub_state = n.advertise<geometry_msgs::PoseStamped>("/hrov/state", 10);
+    ros::Rate rate(10);
 
-    geometry_msgs::Point position;
+    ros::Time t0 = ros::Time::now();
     while (ros::ok())
     {
+        double th = (ros::Time::now() - t0).toSec() * 0.01;
+        // std::cout << "time = " << th << std::endl ; 
         // Set a random position to be sent in NED Format
-        position.x = randn(4290750, 1.0);
-        position.y = randn(491850, 1.0);
-        position.z = randn(2, 0.5);
-
+        geometry_msgs::PoseStamped pose = trajSimulator(10, th);
         // Set a hello world msg to be sent
-        talker_pub.publish(position);
+        pub_state.publish(pose);
 
         ros::spinOnce();
-        loop_rate.sleep();
+        rate.sleep();
     }
     return 0;
 }
